602a9abfa718ef1c8bf96fd29c2737214c529e64
StanPlatinum <lpjlwjlpjlwj@126.com>
2020-11-13 00:17:21 -0500

    test trts util

--- sdk_test/Enclave/Enclave.cpp
+++ sdk_test/Enclave/Enclave.cpp
@@ -6,8 +6,12 @@
 
 #include "string.h"
 
+#include "trts_util.h"
+#include "trts_emodpr.h"
+
 /* re-define printf inside enclave */
-void printf(const char *fmt, ...){
+void printf(const char *fmt, ...)
+{
 	char buf[BUFSIZ] = {'\0'};
 	va_list ap;
 	va_start(ap, fmt);
@@ -16,10 +20,25 @@ void printf(const char *fmt, ...){
 	Ocall_PrintString(buf);
 }
 
-void Ecall_SomeDataProcessing(char *buf, size_t len){
+void Ecall_SomeDataProcessing(char *buf, size_t len)
+{
 	const char *secret = "I am a secret inside enclave!";
-	if(len > strlen(secret))
+	if (len > strlen(secret))
 	{
-		memcpy(buf, secret, strlen(secret)+1);
+		memcpy(buf, secret, strlen(secret) + 1);
 	}
+
+	//test
+	printf("enclave size: 0x%x\n", get_enclave_size());
+	printf("enclave end: 0x%x\n", get_enclave_end());
+
+	printf("heap size: 0x%x\n", get_heap_size());
+	printf("heap min size: 0x%x\n", get_heap_min_size());
+
+	printf("rsrv size: 0x%x\n", get_rsrv_size());
+	printf("rsrv end: 0x%x\n", get_rsrv_end());
+	printf("rsrv min size: 0x%x\n", get_rsrv_min_size());
+	
+	if (is_stack_addr(buf, len))
+		printf("buf is in stack!");;
 }
